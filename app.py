aad7f5c4fef98944b04c339e3404a8d0bc240bb2
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2025-03-02 11:14:52 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+persons = [
+    {
+        "id": 1,
+        "full_name": "Bill Gates",
+        "company": "Microsoft",
+        "salary": 9283982398
+    },
+    {
+        "id": 2,
+        "full_name": "Bill Gates",
+        "company": "Microsoft",
+        "salary": 9283982398
+    },
+    {
+        "id": 3,
+        "full_name": "Bill Gates",
+        "company": "Microsoft",
+        "salary": 9283982398
+    }
+]
